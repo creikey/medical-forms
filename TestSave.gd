5beaff932c598e9a44b70bf9f5b0060bc3a9bd3b
Cameron Reikes <cameronreikes@gmail.com>
2018-05-22 22:13:25 -0700

    Fix bug where forms wouldn't save

--- TestSave.gd
+++ TestSave.gd
@@ -3,22 +3,20 @@ extends Node2D
 export (PackedScene) var original_form
 
 func _on_SaveButton_pressed():
-	ensure_responses_dir()
 	if($StaticUI/NameEdit.text == ""):
 		return
-	var packed_form = PackedScene.new()
-	var current_form = get_form_node()
-	packed_form.pack(current_form)
-	ResourceSaver.save("user://responses/" + $StaticUI/NameEdit.text + ".tscn", packed_form)
-	current_form.queue_free()
+	var current_form = PackedScene.new()
+	current_form.pack($Form)
+	ResourceSaver.save("res://responses/" + $StaticUI/NameEdit.text + ".tscn", current_form)
+	$Form.queue_free()
 	var new_form = original_form.instance()
-	new_form.name = "Form"
 	add_child(new_form)
+	new_form.name = "Form"
 	$StaticUI/NameEdit.text = ""
 	update_file_list()
 
 func update_file_list():
-	var responses = get_files_in_dir("user://responses/")
+	var responses = get_files_in_dir("res://responses/")
 	$StaticUI/FileList.text = ""
 	for r in responses:
 		$StaticUI/FileList.text += r + "\n"
@@ -37,15 +35,3 @@ func get_files_in_dir(in_dir):
 			files.append(file)
 	dir.list_dir_end()
 	return files
-
-func get_form_node():
-	for n in get_children():
-		if(n.is_in_group("forms")):
-			return n
-
-func ensure_responses_dir():
-	var dir = Directory.new()
-	dir.open("user://")
-	if(dir.dir_exists("responses")):
-		return
-	dir.make_dir("responses")
